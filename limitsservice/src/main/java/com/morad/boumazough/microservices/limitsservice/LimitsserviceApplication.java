a1b9880831695680fd04cc16298c72b685d40477
Anas Rayadi <anas.ryadi@skylark.ma>
2025-09-03 21:21:38 +0100

    create limits service

---
+++ limitsservice/src/main/java/com/morad/boumazough/microservices/limitsservice/LimitsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.morad.boumazough.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsserviceApplication.class, args);
+	}
+
+}
---
+++ limitsservice/src/test/java/com/morad/boumazough/microservices/limitsservice/LimitsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morad.boumazough.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
